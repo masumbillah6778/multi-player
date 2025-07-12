6741c401cb0c3465fb90f9a37465dd44c82df2e0
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-07-12 20:12:56 +0600

    Update bangladesh-love-music-list.js

--- assets/js/bangladesh-love-music-list.js
+++ assets/js/bangladesh-love-music-list.js
@@ -15,48 +15,48 @@ let allMusic = [
     name: "৩. The Cradle of Your Soul",
     artist: "lemonmusicstudio",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby jan jan bole.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "৪. Happy Acoustic Guitar Background Music",
     artist: "Music_Unlimited",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "৫. Sleepy cat",
     artist: "Lofi_hour",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "৬. Cinematic Fairy Tale Story",
     artist: "Daddy_s_Music",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "৭. Relaxed Vlog (Night Street)",
     artist: "Ashot-Danielyan-Composer",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "৮. Summer Walk",
     artist: "Olexy",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "৯. Happy Day",
     artist: "Top-Flow-Production",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   },
   {
     name: "১০. Epicaly",
     artist: "LiteSaturation",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
   }
 ];
