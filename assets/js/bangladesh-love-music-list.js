df18e474160e7197791ca86d07eb41432326f22d
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-07-12 19:59:25 +0600

    Update bangladesh-love-music-list.js

--- assets/js/bangladesh-love-music-list.js
+++ assets/js/bangladesh-love-music-list.js
@@ -3,58 +3,58 @@ let allMusic = [
     name: "১. পিরিত ভিষন জ্বালা",
     artist: "আকাশ মাহমুদ",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
-    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/prem-roshiya-hobo.mp3"
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/tomare-tomare.mp3"
   },
   {
-    name: "2. Ambient Classical Guitar",
+    name: "২. Ambient Classical Guitar",
     artist: "William_King",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "3. The Cradle of Your Soul",
+    name: "৩. The Cradle of Your Soul",
     artist: "lemonmusicstudio",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "4. Happy Acoustic Guitar Background Music",
+    name: "৪. Happy Acoustic Guitar Background Music",
     artist: "Music_Unlimited",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "5. Sleepy cat",
+    name: "৫. Sleepy cat",
     artist: "Lofi_hour",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "6. Cinematic Fairy Tale Story",
+    name: "৬. Cinematic Fairy Tale Story",
     artist: "Daddy_s_Music",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "7. Relaxed Vlog (Night Street)",
+    name: "৭. Relaxed Vlog (Night Street)",
     artist: "Ashot-Danielyan-Composer",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "8. Summer Walk",
+    name: "৮. Summer Walk",
     artist: "Olexy",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "9. Happy Day",
+    name: "৯. Happy Day",
     artist: "Top-Flow-Production",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
   },
   {
-    name: "10. Epicaly",
+    name: "১০. Epicaly",
     artist: "LiteSaturation",
     img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
     src: "https://github.com/masumbillah6778/music_lyrics/raw/main/pirit-vison-jala.mp3"
