ee2467607ede6905cd205b77e8a98c566faedba0
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-07-13 08:59:17 +0600

    Add files via upload

---
+++ assets/js/indian-love-music-list.js
@@ -0,0 +1,122 @@
+let allMusic = [
+  {
+    name: "১. পিরিত ভিষন জ্বালা",
+    artist: "আকাশ মাহমুদ",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/tomare-tomare.mp3"
+  },
+  {
+    name: "২. Ambient Classical Guitar",
+    artist: "William_King",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/tukro-tukro.mp3"
+  },
+  {
+    name: "৩. The Cradle of Your Soul",
+    artist: "lemonmusicstudio",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "৪. Happy Acoustic Guitar Background Music",
+    artist: "Music_Unlimited",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/ai-tumi-sey-tumi.mp3"
+  },
+  {
+    name: "৫. Sleepy cat",
+    artist: "Lofi_hour",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/ak-bindu-valobasa-dau.mp3"
+  },
+  {
+    name: "৬. Cinematic Fairy Tale Story",
+    artist: "Daddy_s_Music",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/amar-jibon-naye-bondhu.mp3"
+  },
+  {
+    name: "৭. Relaxed Vlog (Night Street)",
+    artist: "Ashot-Danielyan-Composer",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/asi-asi-prem.mp3"
+  },
+  {
+    name: "৮. Summer Walk",
+    artist: "Olexy",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/hridoyer-albume-chobi-chilo-jar.mp3"
+  },
+  {
+    name: "৯. Happy Day",
+    artist: "Top-Flow-Production",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/jan-korban.mp3"
+  },
+  {
+    name: "১০. পিরিত ভিষন জ্বালা",
+    artist: "আকাশ মাহমুদ",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/je-amar-mon-kereche.mp3"
+  },
+  {
+    name: "১১. পিরিত ভিষন জ্বালা",
+    artist: "আকাশ মাহমুদ",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/na-na-korbo-na-prem.mp3"
+  },
+  {
+    name: "১২. Ambient Classical Guitar",
+    artist: "William_King",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/rupali-raat.mp3"
+  },
+  {
+    name: "১৩. The Cradle of Your Soul",
+    artist: "lemonmusicstudio",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/toke-onek-valobashi.mp3"
+  },
+  {
+    name: "১৪. Happy Acoustic Guitar Background Music",
+    artist: "Music_Unlimited",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "১৫. Sleepy cat",
+    artist: "Lofi_hour",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "১৬. Cinematic Fairy Tale Story",
+    artist: "Daddy_s_Music",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "১৭. Relaxed Vlog (Night Street)",
+    artist: "Ashot-Danielyan-Composer",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "১৮. Summer Walk",
+    artist: "Olexy",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "১৯. Happy Day",
+    artist: "Top-Flow-Production",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  },
+  {
+    name: "২০. Epicaly",
+    artist: "LiteSaturation",
+    img: "https://github.com/masumbillah6778/image/raw/main/tania-thumbnail.jpg",
+    src: "https://github.com/masumbillah6778bd/bangladesh-love-music/raw/main/baby-jan-jan-bole.mp3"
+  }
+];
