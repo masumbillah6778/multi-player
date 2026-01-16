2a1f337bf76e4a1e8dc6b20cac97ffc7de003c05
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-16 15:37:10 +0600

    Add files via upload

---
+++ assets/js/music-list/akash-mahmud-music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "1. Harley Bird - Home",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "2. Ikson Anywhere – Ikson",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "3. Beauz & Jvna - Crazy",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+  {
+    name: "4. Lost Sky - Vision NCS",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+];
