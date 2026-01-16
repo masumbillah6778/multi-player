1739795ed7c73fbd2aa3a7ebb7bb49f4f000427b
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-16 18:03:17 +0600

    Update akash-mahmud-music-list.js

--- assets/js/music-list/akash-mahmud-music-list.js
+++ assets/js/music-list/akash-mahmud-music-list.js
@@ -23,5 +23,55 @@ let allMusic = [
     img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
     src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/akkhan-bandha-premer-ghor.mp3",
   },
+  {
+    name: "৫. মাহে রমজান",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/borolok-er-maiya.mp3",
+  },
+  {
+    name: "৬. মাহে",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/dil-dorodia.mp3",
+  },
+  {
+    name: "৭. মাহে ",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/ek-mon-tomi-koijonere.mp3",
+  },
+  {
+    name: "৮. মাহ",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/eklar-sonshar.mp3",
+  },
+  {
+    name: "৯. মাহে রমজান",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/gopon-pirit.mp3",
+  },
+  {
+    name: "১০. মাহে",
+    artist: "আকাশ মাহমুদ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/akash-mahmud.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/jadu-korila.mp3",
+  },
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
