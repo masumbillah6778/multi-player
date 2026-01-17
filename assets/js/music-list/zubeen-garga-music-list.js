d528ad38f3ad979bc0bbe0101588d57792635e57
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-17 14:55:23 +0600

    Add files via upload

---
+++ assets/js/music-list/zubeen-garga-music-list.js
@@ -0,0 +1,8 @@
+let allMusic = [
+  {
+    name: "Preya Re Preya Re",
+    artist : "Zubeen Garg",
+    img : "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    music : "https://github.com/masumbillah6778/best-of-zubeen-garg/raw/main/piya-re-piya-re.mp3",
+  },
+];
