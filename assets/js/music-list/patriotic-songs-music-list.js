f32ad29a5f6280ab93274a721baf4134b4ee9406
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-19 15:36:56 +0600

    Add files via upload

---
+++ assets/js/music-list/patriotic-songs-music-list.js
@@ -0,0 +1,10 @@
+let allMusic = [
+  {
+    name: "1. Harley Bird - Home",
+    artist: "জুবিন গার্গ",
+    img: "https://masumbillah6778.github.io/images/thumbnail/zubeen-garg.jpg",
+    src: "https://github.com/masumbillah6778bd/akash-mahmud/raw/main/64-jelar-gaan.mp3",
+  },
+];
+
+
